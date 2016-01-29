8556a201ae5f74397a5d2b1fa9bd363c9a2ddff1
kioke takaaki <taka.vao.321@gmail.com>
2016-01-29 21:11:59 +0900

    非推奨メソッドの使用を修正
    コレクション名をとれるようになった

--- test/mongodb/testMongoDb.java
+++ test/mongodb/testMongoDb.java
@@ -1,12 +1,20 @@
 import com.mongodb.*;
+import com.mongodb.client.*;
 
 public class testMongoDb {
 
     public static void main(String[] args) {
         try {
             MongoClient mongo = new MongoClient("localhost", 27017);
-            DB db = mongo.getDB("testchan");
+            MongoDatabase db = mongo.getDatabase("testchan");   
             System.out.println("Connected");
+
+            MongoIterable<String> colls = db.listCollectionNames();
+            MongoCursor<String> itr = colls.iterator();
+            System.out.print("COLLECTIONS: ");
+            for(String s : colls ){
+                System.out.println(" > > >" + s);
+            }
 
             mongo.close();
         } catch (Exception e) {
