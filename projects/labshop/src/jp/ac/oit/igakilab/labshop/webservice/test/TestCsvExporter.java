22e333b56d4ea32f32158d92f02cc9d4b50a3e34
kioke takaaki <taka.vao.321@gmail.com>
2016-02-18 23:41:34 +0900

    MemberDB動作確認 #8

--- projects/labshop/src/jp/ac/oit/igakilab/labshop/webservice/test/TestCsvExporter.java
+++ projects/labshop/src/jp/ac/oit/igakilab/labshop/webservice/test/TestCsvExporter.java
@@ -3,7 +3,9 @@ package jp.ac.oit.igakilab.labshop.webservice.test;
 import java.io.IOException;
 
 import jp.ac.oit.igakilab.labshop.dbcontroller.ItemDBController;
+import jp.ac.oit.igakilab.labshop.dbcontroller.MemberDBController;
 import jp.ac.oit.igakilab.labshop.dbcontroller.export.ItemDBCsvExporter;
+import jp.ac.oit.igakilab.labshop.dbcontroller.export.MemberDBCsvExporter;
 
 public class TestCsvExporter {
 	public void exportItemDB()
@@ -19,6 +21,22 @@ public class TestCsvExporter {
 		ItemDBController dbc = new ItemDBController();
 		ItemDBCsvExporter exporter = new ItemDBCsvExporter(dbc);
 		exporter.importCsv("/var/log/tomcat7/labshop/itemdb.csv");
+		dbc.close();
+	}
+
+	public void exportMemberDB()
+	throws IOException{
+		MemberDBController dbc = new MemberDBController();
+		MemberDBCsvExporter exporter = new MemberDBCsvExporter(dbc);
+		exporter.exportCsv("/var/log/tomcat7/labshop/memberdb.csv");
+		dbc.close();
+	}
+
+	public void importMemberDB()
+	throws IOException{
+		MemberDBController dbc = new MemberDBController();
+		MemberDBCsvExporter exporter = new MemberDBCsvExporter(dbc);
+		exporter.importCsv("/var/log/tomcat7/labshop/memberdb.csv");
 		dbc.close();
 	}
 }
