9fe897b40ed5463f2806fec7f8a38bded5729dc0
kioke takaaki <taka.vao.321@gmail.com>
2016-02-23 23:37:43 +0900

    monthlymember
    作りかけ

--- projects/labshop/WebContent/js/LabshopUtil.js
+++ projects/labshop/WebContent/js/LabshopUtil.js
@@ -7,3 +7,13 @@ LabshopUtil.createLabelsRow = function(labels){
 	}
 	return row;
 }
+
+LabshopUtil.addMonthOptions(jquery_object){
+	for(var i=1; i<=12; i++){
+		jquery_object.append(
+			$("<option></option>")
+				.attr("value", i)
+				.text(i)
+		);
+	}
+}
